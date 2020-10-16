9cc800d41b6e41ee829b6f3d3cdba40b2f92c9c8
Jacob Dowdy <jacobdowdy@rocketmail.com>
2020-10-16 14:27:47 -0400

    Create for-loops.cs

---
+++ for-loops.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Dynamic;
+
+namespace CSharpFundamentals
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "John Smith";
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
