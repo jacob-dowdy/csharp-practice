6781791d88fa8332c03e2f535dc9fd26c23b8407
Jacob Dowdy <jacobdowdy@rocketmail.com>
2020-10-16 16:51:24 -0400

    Create divisibleBy3.cs

---
+++ divisibleBy3.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Dynamic;
+
+namespace CSharpFundamentals
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. 
+            // Display the count on the console.
+            var count = new Int32();
+
+            for (var i = 1; i <= 100; i++)
+            {
+                if(i%3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
